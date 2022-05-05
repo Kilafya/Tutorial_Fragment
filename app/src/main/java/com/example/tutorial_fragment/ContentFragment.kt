5c369ccc8076439e612a9d15d8108e99a7c77c65
drayl <sunrise.161@mail.ru>
2022-05-05 21:01:15 +0300

    create first fragment

---
+++ app/src/main/java/com/example/tutorial_fragment/ContentFragment.kt
@@ -0,0 +1,59 @@
+package com.example.tutorial_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.tutorial_fragment.databinding.FragmentContentBinding
+
+class ContentFragment : Fragment() {
+
+    private lateinit var binding: FragmentContentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentContentBinding.inflate(inflater, container, false)
+        with (binding) {
+            tvScreenNumber.text = "Screen № ${getCounterValue()}"
+            tvQuote.text = getQuote()
+            btnNext.setOnClickListener { launchNext() }
+            btnBack.setOnClickListener { requireActivity().onBackPressed() }
+        }
+        return binding.root
+    }
+
+    private fun launchNext() {
+        val fragment = newInstance(
+            counterValue = (requireActivity() as MainActivity).getScreenNumber() + 1,
+            quote = (requireActivity() as MainActivity).createQuote()
+        )
+        parentFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    private fun getCounterValue() = requireArguments().getInt(KEY_COUNTER_VALUE)
+
+    private fun getQuote(): String = requireArguments().getString(KEY_QUOTE) ?: "Quote not found"
+
+    companion object {
+        const val KEY_COUNTER_VALUE = "COUNTER_VALUE"
+        const val KEY_QUOTE = "QUOTE"
+
+        fun newInstance(counterValue: Int, quote: String): ContentFragment {
+            val args = Bundle().apply {
+                putInt(KEY_COUNTER_VALUE, counterValue)
+                putString(KEY_QUOTE, quote)
+            }
+            val fragment = ContentFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
--- app/src/main/java/com/example/tutorial_fragment/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_fragment/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tutorial_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tutorial_fragment.databinding.ActivityMainBinding
+import com.github.javafaker.Faker
 
 class MainActivity : AppCompatActivity() {
+
+    private val faker = Faker.instance()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val fragment = ContentFragment.newInstance(
+                counterValue = 1,
+                quote = createQuote()
+            )
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
+
+    fun createQuote(): String = faker.harryPotter().quote()
+
+    fun getScreenNumber() = supportFragmentManager.backStackEntryCount + 1
 }
